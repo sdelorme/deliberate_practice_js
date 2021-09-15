3165f32326f11c017c61ff38acc97dda558d56ba
Seth Delorme <delorme.s.m@gmail.com>
2021-09-15 13:48:18 -0500

    updates numberGuesser functions

--- numberGuesser.js
+++ numberGuesser.js
@@ -3,11 +3,11 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 // Write your code below:
 const generateTarget = () => {
-  return Math.floor(Math.random() * 9);
+  return Math.floor(Math.random() * 10);
 };
 const compareGuesses = (userGuess, computerGuess, targetNumber) => {
-  let userDifference = Math.abs(userGuess - targetNumber);
-  let computerDifference = Math.abs(computerGuess - targetNumber);
+  const userDifference = Math.abs(userGuess - targetNumber);
+  const computerDifference = Math.abs(computerGuess - targetNumber);
   if (userDifference <= computerDifference) {
     return true;
   } else {
