a61a124964f6b02f3c7dfd46ea7b0c34eaac5245
Seth Delorme <delorme.s.m@gmail.com>
2021-09-21 14:50:44 -0500

    array prac deliberate practice

---
+++ arrayPrac1.js
@@ -0,0 +1,18 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+// console.log(secretMessage.length);
+secretMessage.pop();
+// console.log(secretMessage.length);
+// console.log(secretMessage);
+secretMessage.push('to', 'Program');
+// console.log(secretMessage);
+
+secretMessage[7] = 'right';
+// console.log(secretMessage[7]);
+secretMessage.shift();
+secretMessage.unshift('Programming');
+
+// console.log(secretMessage);
+secretMessage.splice(6, 5, 'know');
+// console.log(secretMessage);
+console.log(secretMessage.join(' '));
