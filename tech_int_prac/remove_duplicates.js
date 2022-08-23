84ebc34733d94aaf252e5d3ba4cdd4618bd852e0
Seth Delorme <delorme.s.m@gmail.com>
2022-08-23 12:15:03 -0400

    update remove duplicates solution -- now working on leetcode

--- tech_int_prac/remove_duplicates.js
+++ tech_int_prac/remove_duplicates.js
@@ -18,20 +18,56 @@
 
 // remove_duplicates([0,0,1,1,1,2,2,3,3,4])
 
+
 var removeDuplicates = function(nums) {
-  let duplicates = {};
-  let result = [];
-  for (let i = 0; i < nums.length; i++) {
-    if (duplicates[nums[i]] === nums[i]) {
-      continue;
-    } else {
-      duplicates[nums[i]] = nums[i];
-      result.push(nums[i]);
+  let i = 0;
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[j] != nums[i]) {
+      nums[++i] = nums[j];
     }
-    console.log(duplicates, result);
   }
-  let k = result.length;
-  return k;
+  return ++i;
 };
 
-console.log(removeDuplicates([1,1,2]));
+// [0,0,1,1,1,2,2,3,3,4]
+// numsi = 0 [0]
+// numsj = 0 [1]
+
+// [0,1,1,1,1,2,2,3,3,4]
+// numsi = 0 [1]
+// numsj = 1 [2]
+
+// [0,1,1,1,1,2,2,3,3,4]
+// numsi = 1 [1]
+// numsj = 1 [3]
+
+// [0,1,1,1,1,2,2,3,3,4]
+// numsi = 1 [1]
+// numsj = 1 [4]
+
+// [0,1,2,1,1,2,2,3,3,4]
+//            ^ 
+// numsi = 1 [2]
+// numsj = 2 [5]
+
+// [0,1,2,1,1,2,2,3,3,4]
+//              ^ 
+// numsi = 2 [2]
+// numsj = 2 [6]
+
+// [0,1,2,3,1,2,2,3,3,4]
+//                ^ 
+// numsi = 2 [3]
+// numsj = 3 [7]
+
+// [0,1,2,3,1,2,2,3,3,4]
+//                  ^ 
+// numsi = 3 [3]
+// numsj = 3 [8]
+
+// [0,1,2,3,4,2,2,3,3,4]
+//                    ^ 
+// numsi = 3 [4]
+// numsj = 4 [9]
+
+// return ++i = 5
